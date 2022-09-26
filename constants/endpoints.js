666f71e60f9f1d6967da818ebbef0bc99413bd03
Avaer Kazmer <a@modules.io>
2022-09-26 17:33:48 -0400

    Update endpoint urls

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -1,4 +1,4 @@
 export const weaviateUrl = `https://weaviate.weabaverse.com`;
-export const stableDiffusionUrl = `https://stable-diffusion-cw.webaverse.com/`;
-export const voiceUrl = `https://voice-cw.webaverse.com/`;
-export const diffsoundUrl = `https://diffsound.webaverse.com/`;
+export const stableDiffusionUrl = `https://stable-diffusion.webaverse.com`;
+export const voiceUrl = `https://voice-cw.webaverse.com`;
+export const diffsoundUrl = `https://diffsound.webaverse.com`;
