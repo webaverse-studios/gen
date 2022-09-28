7c8617ee2183b108731ba61ef7508d0c775b400c
Avaer Kazmer <a@modules.io>
2022-09-28 11:04:55 -0400

    Update scripts

--- scripts/ensure-database-schemas.js
+++ scripts/ensure-database-schemas.js
@@ -1,53 +1,27 @@
 const weaviate = require('weaviate-client');
+const schemas = require('../schemas/schemas.js');
 
 const client = weaviate.client({
   scheme: 'http',
   host: 'weaviate-server.webaverse.com',
 });
 
-const schemas = [
-  {
-    "class": "Content",
-    "description": "Webaverse content",
-    "properties": [
-      {
-        "dataType": [
-          "string"
-        ],
-        "description": "Title of the page",
-        "name": "title"
-      },
-      {
-        "dataType": [
-          "string"
-        ],
-        "description": "Content of the page",
-        "name": "content"
-      },
-      {
-        "dataType": [
-          "string"
-        ],
-        "description": "Type of content",
-        "name": "type"
-      },
-    ],
-  },
-];
-
 (async () => {
-  await client
+  /* await client
     .schema
     .getter()
-    .do();
+    .do(); */
   for (const schema of schemas) {
     try {
       await client.schema
         .classCreator()
         .withClass(schema)
         .do();
+      console.log(`created ${schema.class}`);
     } catch(err) {
-      if (!/422/.test(err)) { // already exists
+      if (/422/.test(err)) { // already exists
+        console.log(`already exists ${schema.class}`);
+      } else {
         throw err;
       }
     }
