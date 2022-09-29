93bbc60881b04cfaba2b53883a21b67945771f7b
Avaer Kazmer <a@modules.io>
2022-09-29 16:56:59 -0400

    Update item generator

--- generators/image/item.js
+++ generators/image/item.js
@@ -2,5 +2,5 @@ import {generateImage} from './generate-image.js';
 
 export const generateItemImage = generateImage({
   modelName: 'webaverse_items',
-  prefix: `Anime RPG video game item concept art, tranding on ArtStation.`,
+  prefix: `Video game item concept render, trending on ArtStation.`,
 });
