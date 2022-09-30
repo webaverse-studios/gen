d5b523b4dc200b59ac29e47e0f3a9eb0ecc63753
Avaer Kazmer <a@modules.io>
2022-09-30 17:04:01 -0400

    Update image generators

--- generators/image/generate-image.js
+++ generators/image/generate-image.js
@@ -8,11 +8,17 @@ export const generateImage = ({
   description,
 } = {}) => {
   const s = `${prefix} ${description}`;
-  const u = `${stableDiffusionUrl}/image?s=${s}&model=${modelName}`;
+  const u = `${stableDiffusionUrl}/image?s=${encodeURIComponent(s)}&model=${modelName}`;
+  console.log('generate image url 1', {u});
   const res = await fetch(u);
+  console.log('generate image url 2', {u, status: res.status});
   if (res.ok) {
     const arrayBuffer = await res.arrayBuffer();
-    return arrayBuffer;
+    if (arrayBuffer.byteLength > 0) {
+      return arrayBuffer;
+    } else {
+      throw new Error(`generated empty image`);
+    }
   } else {
     throw new Error(`invalid status: ${res.status}`);
   }
