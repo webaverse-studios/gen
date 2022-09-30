35455870137d162d0d8ac8b2c1db4fd2b4450d62
Avaer Kazmer <a@modules.io>
2022-09-30 13:43:38 -0400

    Add generate endpoint

---
+++ pages/api/test/generate/[schemaName]/[name]/[attributeName].js
@@ -0,0 +1,45 @@
+import {DatasetEngine} from '../../../../../../datasets/datasets.js';
+import {Ctx} from '../../../../../../context.js';
+import datasets from '../../../../../../datasets/data.js';
+
+export default async (req, res) => {
+  // match /api/:schemaName/:name/:attributeName with regex
+  const match = req.url.match(/^\/api\/test\/generate\/([^\/]+?)\/([^\/]+?)\/([^\/]+?)$/);
+  if (match) {
+    // decode the match
+    const schemaName = decodeURIComponent(match[1]);
+    const name = decodeURIComponent(match[2]);
+    let attributeName = decodeURIComponent(match[3]);
+    if (attributeName === 'description') {
+      attributeName = '';
+    }
+
+    console.log('generate prompt', {
+      schemaName,
+      name,
+      attributeName,
+    });
+
+    const dataset = datasets[schemaName];
+    if (dataset) {
+      const prompt = dataset.generatePrompt(name, attributeName);
+
+      const c = new Ctx();
+      const datasetEngine = new DatasetEngine({
+        dataset,
+        aiClient: c.aiClient,
+      });
+      // console.log('got prompt', {prompt});
+      
+      const response = await datasetEngine.generateItemAttribute('Annie Masuki', attributeName);
+      const result = `${prompt}${attributeName ? ' ' : ''}${response}`;
+      console.log('got response', {prompt, response, result});
+      
+      res.send(result);
+    } else {
+      res.send(404);
+    }
+  } else {
+    res.send(400);
+  }
+};
