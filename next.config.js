6754638f3c3e0ac725017c6122f75eb18957674c
Avaer Kazmer <a@modules.io>
2022-09-30 12:40:40 -0400

    Update next confix with raw loader

--- next.config.js
+++ next.config.js
@@ -14,6 +14,28 @@ if (
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    return Object.assign({}, config, {
+      /* externals: Object.assign({}, config.externals, {
+        fs: 'fs',
+      }), */
+      module: Object.assign({}, config.module, {
+        rules: config.module.rules.concat([
+          /* {
+            test: /\.md$/,
+            loader: 'emit-file-loader',
+            options: {
+              name: 'dist/[path][name].[ext]',
+            },
+          }, */
+          {
+            test: /\.md$/,
+            loader: 'raw-loader',
+          }
+        ]),
+      }),
+    });
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
