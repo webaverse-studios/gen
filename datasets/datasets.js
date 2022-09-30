92072cbb249888a8d999eb752d5a933b93d3032a
Avaer Kazmer <a@modules.io>
2022-09-30 08:56:35 -0400

    More dataset test debugging

--- datasets/datasets.js
+++ datasets/datasets.js
@@ -3,62 +3,72 @@ const Alea = require('alea');
 export class Dataset {
   constructor({
     prefix,
-    parser,
+    parse,
     n = 8, // number of examples randomly selected per prompt
     items,
   }) {
     this.prefix = prefix;
-    this.parser = parser;
+    this.parser = parse;
     this.n = n;
     this.items = [];
-    this.parsedItems = [];
 
     if (items) {
       this.addItems(items);
     }
   }
   addItems(items) {
-    this.items = items;
-    this.#refreshItems();
+    this.items = items.map(itemString => {
+      itemString = itemString.trim();
+      const parsedItem = this.parser(itemString);
+      if (!parsedItem) {
+        throw new Error(`failed to parse item: ${JSON.stringify(itemString)}`);
+      }
+      return parsedItem;
+    });
   }
-  #refreshItems() {
-    this.parsedItems = this.items.map(item => this.parser(item));
-  }
-  #formatItem(item) {
-    return `\
-# ${item.name}
-${this.#formatAttributes(item)}`
-  }
-  #formatAttributes(item) {
-    const keys = Object.keys(item);
-    let s = '';
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      if (key !== 'name') {
+  #formatItem(item, attributeName) {
+    const formatAttributes = item => {
+      const keys = Object.keys(item);
+      let s = '';
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
         if (s.length > 0) {
           s += '\n';
         }
         s += `${key}${item[key]}`;
       }
-    }
-    return s;
+      return s;
+    };
+
+    return `\
+# ${item.name}
+${attributeName} ${item[attributeName]}`
   }
   generatePrompt(name, attributeName) {
+    if (!name) {
+      throw new Error('name is required');
+    }
+    if (!attributeName) {
+      throw new Error('attributeName is required');
+    }
+    
     const rng = new Alea(name);
 
-    const candidateItems = this.items.slice();
+    const candidateItems = this.items.filter(item => item[attributeName]);
     const localItems = [];
     for (let i = 0; i < this.n && candidateItems.length > 0; i++) {
       const index = Math.floor(rng() * candidateItems.length);
       localItems.push(candidateItems[index]);
       candidateItems.splice(index, 1);
     }
+
+    // console.log('got local items', {localItems});
     
     return `\
 ${this.prefix}
 
 ${localItems.map(item =>
-  this.#formatItem(item)
+  this.#formatItem(item, attributeName)
 ).join('\n\n')}
 
 # ${name}
