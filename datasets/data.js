5b2d5dd7251c1dec66c6bef0f2b2b9e160111995
Avaer Kazmer <a@modules.io>
2022-09-30 12:42:43 -0400

    Add data.js

---
+++ datasets/data.js
@@ -0,0 +1,9 @@
+import characters from './parsers/characters.js';
+import settings from './parsers/settings.js';
+import items from './parsers/items.js';
+
+export default {
+  characters,
+  settings,
+  items,
+};
