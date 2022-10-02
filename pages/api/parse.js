98b6538ffefc6d394e9fbb00f603241b8ef46f10
Avaer Kazmer <a@modules.io>
2022-10-02 11:07:21 -0400

    Major prompting checkpont

--- pages/api/parse.js
+++ pages/api/parse.js
@@ -1,29 +1,68 @@
 import charactersMd from '../../datasets/data/characters.md';
 import settingsMd from '../../datasets/data/settings.md';
 import itemsMd from '../../datasets/data/items.md';
-import {capitalizeAllWords} from '../../utils.js';
+import cutscenesMd from '../../datasets/data/cutscenes.md';
+import chatsMd from '../../datasets/data/chats.md';
+import bantersMd from '../../datasets/data/banters.md';
+import {capitalizeAllWords, isAllCaps} from '../../utils.js';
 
 const _hasNewline = s => s.indexOf('\n') !== -1;
 
-const _formatItem = item => {
+const _formatItemText = (item, ignoreKeys = []) => {
   let s = '';
   for (const k in item) {
-    const v = item[k];
-    if (s) {
-      s += '\n';
+    if (!ignoreKeys.includes(k)) {
+      const v = item[k];
+      if (s) {
+        s += '\n';
+      }
+      s += `${capitalizeAllWords(k)}: ${_hasNewline(v) ? '\n' : ''}${v}`;
     }
-    s += `${capitalizeAllWords(k)}: ${_hasNewline(v) ? '\n' : ''}${v}`;
   }
   return s;
+};
+const _formatItemJson = item => {
+  const prompt = `Type: ${item['Type']}\nName: ${item['Name']}\nDescription: ${item['Description']})`;
+  const completion = _formatItemText(item, ['Type', 'Name', 'Description']);
+  return {
+    prompt,
+    completion,
+  };
 };
 
 export default async (req, res) => {
   let items = [];
   try {
-    for (let md of [
-      charactersMd,
-      settingsMd,
-      itemsMd,
+    for (let {
+      type,
+      md,
+      groupKey = null,
+    } of [
+      {
+        type: 'Character',
+        md: charactersMd,
+      },
+      {
+        type: 'Setting',
+        md: settingsMd,
+      },
+      {
+        type: 'Item',
+        md: itemsMd,
+      },
+      {
+        type: 'Cutscene',
+        md: cutscenesMd,
+      },
+      {
+        type: 'Chat',
+        md: chatsMd,
+      },
+      {
+        type: 'Banter',
+        md: bantersMd,
+        groupKey: 'Banters',
+      },
     ]) {
       const match = md.match(/^([\s\S]*?)\n\n([\s\S]*)$/);
       if (match) {
@@ -36,7 +75,7 @@ export default async (req, res) => {
         let match2;
         while (match2 = r.exec(md)) {
           const itemString = match2[1];
-          console.log('itemString', itemString);
+          // console.log('itemString', itemString);
 
           const itemAttributes = {};
           let currentAttributeName = '';
@@ -48,12 +87,15 @@ export default async (req, res) => {
             currentAttributeValue = '';
           };
 
+          // initialize with type
+          itemAttributes['Type'] = type;
+
           const itemLines = itemString.split('\n');
           for (let i = 0; i < itemLines.length; i++) {
             const itemLine = itemLines[i];
 
             const match3 = itemLine.match(/^([\s\S]+?):(?: )?(.*)(?:\n|$)/);
-            if (match3) {
+            if (match3 && !isAllCaps(match3[1])) {
               if (currentAttributeName) {
                 _flushAttribute();
               }
@@ -62,10 +104,17 @@ export default async (req, res) => {
               currentAttributeValue = match3[2];
             } else {
               if (currentAttributeName) {
-                if (currentAttributeValue) {
-                  currentAttributeValue += '\n';
+                if (currentAttributeName === groupKey) {
+                  // console.log('got banter split', {currentAttributeName, currentAttributeValue});
+                  const itemAttributesClone = {...itemAttributes};
+                  itemAttributesClone[currentAttributeName] = itemLine;
+                  items.push(itemAttributesClone);
+                } else {
+                  if (currentAttributeValue) {
+                    currentAttributeValue += '\n';
+                  }
+                  currentAttributeValue += itemLine;
                 }
-                currentAttributeValue += itemLine;
               } else {
                 throw new Error('did not have item attribute context: ' + JSON.stringify({itemString, itemLines}, null, 2));
               }
@@ -87,8 +136,12 @@ export default async (req, res) => {
     }
 
     // res.json(items);
+    
+    // res.send(
+    //   items.map(item => _formatItemText(item)).join('\n\n')
+    // );
     res.send(
-      items.map(item => _formatItem(item)).join('\n\n')
+      items.map(item => JSON.stringify(_formatItemJson(item))).join('\n')
     );
   } catch(err) {
     res.status(500).send(err.stack);
