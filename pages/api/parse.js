1b04a39585574bf894b719c22344c415e2b937c5
Avaer Kazmer <a@modules.io>
2022-10-03 22:47:57 -0400

    More parsing cleanup

--- pages/api/parse.js
+++ pages/api/parse.js
@@ -8,6 +8,8 @@ import bantersMd from '../../datasets/data/banters.md';
 import matchesMd from '../../datasets/data/matches.md'; */
 import {capitalizeAllWords, isAllCaps} from '../../utils.js';
 
+//
+
 const datasetBasePath = `https://webaverse.github.io/lore/datasets/data/`;
 const mdUrls = [
   'characters.md',
@@ -19,8 +21,20 @@ const mdUrls = [
   'matches.md',
 ].map(mdUrl => `${datasetBasePath}${mdUrl}`);
 
-const _hasNewline = s => s.indexOf('\n') !== -1;
+const fetchText = async u => {
+  const res = await fetch(u);
+  if (res.ok) {
+    const text = await res.text();
+    return text;
+  } else {
+    throw new Error(`fetch error ${res.status} ${res.statusText} ${u}`);
+  }
+};
+const mdsPromise = Promise.all(mdUrls.map(mdUrl => fetchText(mdUrl)));
 
+//
+
+const _hasNewline = s => s.indexOf('\n') !== -1;
 const _formatItemText = (item, ignoreKeys = []) => {
   let s = '';
   for (const k in item) {
@@ -48,17 +62,93 @@ ${item[descriptionKey] ? `${descriptionKey}: ${item[descriptionKey]}\n` : ''}\
     completion,
   };
 };
+const parseDatasetSpec = mdSpec => {
+  let {
+    type,
+    md,
+    nameKey = 'Name',
+    descriptionKey = 'Description',
+    groupKey = null,
+  } = mdSpec;
+  const items = [];
 
-const fetchText = async u => {
-  const res = await fetch(u);
-  if (res.ok) {
-    const text = await res.text();
-    return text;
+  const _formatItemJson2 = item => _formatItemJson(item, {
+    nameKey,
+    descriptionKey,
+  });
+
+  const match = md.match(/^([\s\S]*?)\n\n([\s\S]*)$/);
+  if (match) {
+    const prefix = match[1];
+    md = match[2];
+
+    // console.log('prefix', prefix);
+
+    const r = /([\s\S]+?)(?:\n\n|$)/g;
+    let match2;
+    while (match2 = r.exec(md)) {
+      const itemString = match2[1];
+      // console.log('itemString', itemString);
+
+      const itemAttributes = {};
+      let currentAttributeName = '';
+      let currentAttributeValue = '';
+      const _flushAttribute = () => {
+        itemAttributes[currentAttributeName] = currentAttributeValue;
+        
+        currentAttributeName = '';
+        currentAttributeValue = '';
+      };
+
+      // initialize with type
+      itemAttributes['Type'] = type;
+
+      const itemLines = itemString.split('\n');
+      for (let i = 0; i < itemLines.length; i++) {
+        const itemLine = itemLines[i];
+
+        const match3 = itemLine.match(/^([\s\S]+?):(?: )?(.*)(?:\n|$)/);
+        if (match3 && !isAllCaps(match3[1])) {
+          if (currentAttributeName) {
+            _flushAttribute();
+          }
+
+          currentAttributeName = match3[1];
+          currentAttributeValue = match3[2];
+        } else {
+          if (currentAttributeName) {
+            if (currentAttributeName === groupKey) {
+              // console.log('got banter split', {currentAttributeName, currentAttributeValue});
+              const itemAttributesClone = {...itemAttributes};
+              itemAttributesClone[currentAttributeName] = itemLine;
+              const formattedItem = _formatItemJson2(itemAttributesClone);
+              items.push(formattedItem);
+            } else {
+              if (currentAttributeValue) {
+                currentAttributeValue += '\n';
+              }
+              currentAttributeValue += itemLine;
+            }
+          } else {
+            throw new Error('did not have item attribute context: ' + JSON.stringify({itemString, itemLines}, null, 2));
+          }
+        }
+      }
+      if (currentAttributeName) {
+        _flushAttribute();
+      }
+
+      const formattedItem = _formatItemJson2(itemAttributes);
+      items.push(formattedItem);
+    }
   } else {
-    throw new Error(`fetch error ${res.status} ${res.statusText} ${u}`);
+    throw new Error('had no prefix: ' + JSON.stringify(md));
   }
+
+  return items;
 };
-const mdsPromise = Promise.all(mdUrls.map(mdUrl => fetchText(mdUrl)));
+
+//
 
 export default async (req, res) => {
   let items = [];
@@ -73,13 +163,7 @@ export default async (req, res) => {
       matchesMd,
     ] = await mdsPromise;
 
-    for (let {
-      type,
-      md,
-      nameKey = 'Name',
-      descriptionKey = 'Description',
-      groupKey = null,
-    } of [
+    for (let mdSpec of [
       {
         type: 'Character',
         md: charactersMd,
@@ -112,82 +196,8 @@ export default async (req, res) => {
         descriptionKey: 'Match string',
       },
     ]) {
-      const _formatItemJson2 = item => _formatItemJson(item, {
-        nameKey,
-        descriptionKey,
-      });
-      
-      const match = md.match(/^([\s\S]*?)\n\n([\s\S]*)$/);
-      if (match) {
-        const prefix = match[1];
-        md = match[2];
-
-        // console.log('prefix', prefix);
-
-        const r = /([\s\S]+?)(?:\n\n|$)/g;
-        let match2;
-        while (match2 = r.exec(md)) {
-          const itemString = match2[1];
-          // console.log('itemString', itemString);
-
-          const itemAttributes = {};
-          let currentAttributeName = '';
-          let currentAttributeValue = '';
-          const _flushAttribute = () => {
-            itemAttributes[currentAttributeName] = currentAttributeValue;
-            
-            currentAttributeName = '';
-            currentAttributeValue = '';
-          };
-
-          // initialize with type
-          itemAttributes['Type'] = type;
-
-          const itemLines = itemString.split('\n');
-          for (let i = 0; i < itemLines.length; i++) {
-            const itemLine = itemLines[i];
-
-            const match3 = itemLine.match(/^([\s\S]+?):(?: )?(.*)(?:\n|$)/);
-            if (match3 && !isAllCaps(match3[1])) {
-              if (currentAttributeName) {
-                _flushAttribute();
-              }
-
-              currentAttributeName = match3[1];
-              currentAttributeValue = match3[2];
-            } else {
-              if (currentAttributeName) {
-                if (currentAttributeName === groupKey) {
-                  // console.log('got banter split', {currentAttributeName, currentAttributeValue});
-                  const itemAttributesClone = {...itemAttributes};
-                  itemAttributesClone[currentAttributeName] = itemLine;
-                  const formattedItem = _formatItemJson2(itemAttributesClone);
-                  items.push(formattedItem);
-                } else {
-                  if (currentAttributeValue) {
-                    currentAttributeValue += '\n';
-                  }
-                  currentAttributeValue += itemLine;
-                }
-              } else {
-                throw new Error('did not have item attribute context: ' + JSON.stringify({itemString, itemLines}, null, 2));
-              }
-            }
-          }
-          if (currentAttributeName) {
-            _flushAttribute();
-          }
-
-          const formattedItem = _formatItemJson2(itemAttributes);
-          items.push(formattedItem);
-        }
-
-        /* const response = await fetch(md);
-        const text = await response.text();
-        console.log(text); */
-      } else {
-        throw new Error('had no prefix: ' + JSON.stringify(md));
-      }
+      const localItems = parseDatasetSpec(mdSpec);
+      items.push(...localItems);
     }
 
     // res.json(items);
