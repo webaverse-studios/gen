c8a869d3a500529093ec495270c2fd7dd05a785e
Avaer Kazmer <a@modules.io>
2022-10-05 10:27:02 -0400

    Dataset engine dead code cleanup

--- datasets/dataset-engine.js
+++ datasets/dataset-engine.js
@@ -1,155 +1,9 @@
 // import Alea from 'alea';
 import {
-  nameKeySymbol,
-  descriptionKeySymbol,
   formatDatasetNamePrompt,
   formatDatasetDescriptionPrompt,
   formatDatasetAttributePrompts,
 } from './dataset-parser.js';
-
-//
-
-/* export const parseItems = s => {
-  const lines = s.split('\n');
-
-  const items = [];
-  let currentName = '';
-  let currentAttributes = {};
-  const _flushObject = () => {
-    items.push({
-      name: currentName,
-      attributes: currentAttributes,
-    });
-    currentName = '';
-    currentAttributes = {};
-  };
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].trim();
-    if (!currentName) {
-      if (line) {
-        const match = line.match(/^# (.*)$/);
-        if (match) {
-          currentName = match[1];
-        } else {
-          console.log('expected name', {line});
-          throw new Error('expected name: ' + JSON.stringify(line));
-        }
-      } else {
-        continue;
-      }
-    } else {
-      if (line) {
-        const attributeMatch = line.match(/^([a-zA-Z0-9]+?:|[#]+) (.*)$/);
-        let attributeName, attributeValue;
-        if (attributeMatch) {
-            attributeName = attributeMatch[1];
-            attributeValue = attributeMatch[2];
-        } else {
-          attributeName = '';
-          attributeValue = line;
-        }
-        currentAttributes[attributeName] = attributeValue;
-      } else {
-        _flushObject();
-      }
-    }
-  }
-  if (currentName) {
-    _flushObject();
-  }
-  return items;
-};
-export const parseDataset = s => {
-  const lines = s.split('\n');
-
-  const prefix = lines.splice(0, 2).join('\n').trim();
-
-  const suffix = lines.join('\n');
-  const items = parseItems(suffix);
-
-  return {
-    prefix,
-    items,
-  };
-
-};
-
-export const formatItem = item => {
-  return Object.keys(item.attributes).map(attributeName => {
-    const attributeValue = item.attributes[attributeName];
-    return `${attributeName} ${attributeValue}`;
-  }).join('\n');
-};
-
-//
-
-export class Dataset {
-  constructor({
-    prefix,
-    parse,
-    n = 8, // number of examples randomly selected per prompt
-    items,
-  }) {
-    this.prefix = prefix;
-    this.parser = parse;
-    this.n = n;
-    this.items = items;
-  }
-  #formatItem(item) {
-    const formatAttributes = item => {
-      const keys = Object.keys(item);
-      let s = '';
-      for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-        if (s.length > 0) {
-          s += '\n';
-        }
-        s += `${key}${item[key]}`;
-      }
-      return s;
-    };
-
-    return `\
-# ${item.name}
-${Object.keys(item.attributes).map(attributeName => {
-  const attributeValue = item.attributes[attributeName];
-  return `${attributeName} ${item.attributes[attributeName]}`;
-}).join('\n')}`;
-  }
-  #getRngItems(name) {
-    const rng = new Alea(name);
-
-    const candidateItems = this.items.slice();
-    const localItems = [];
-    for (let i = 0; i < this.n && candidateItems.length > 0; i++) {
-      const index = Math.floor(rng() * candidateItems.length);
-      localItems.push(candidateItems[index]);
-      candidateItems.splice(index, 1);
-    }
-    return localItems;
-  }
-  generateItemPrompt(name) {
-    if (typeof name !== 'string') {
-      throw new Error('name is required');
-    }
-    
-    const localItems = this.#getRngItems(name);
-
-    const prompt = `\
-${this.prefix}
-
-${localItems.map(item =>
-  this.#formatItem(item)
-).join('\n\n')}
-
-# ${name}
-##`;
-
-    return prompt;
-  }
-} */
-
-//
 
 export class DatasetEngine {
   constructor({
@@ -196,13 +50,7 @@ export class DatasetEngine {
       attributes[attributeName] = attributeValue;
     }));
 
-    return {
-      [nameKey]: name,
-      [descriptionKey]: description,
-      ...attributes,
-      [nameKeySymbol]: nameKey,
-      [descriptionKeySymbol]: descriptionKey,
-    };
+    return attributes;
 
     /* if (this.dataset.items.length > 0) {
       const item0 = this.dataset.items[0];
