81a1b8397b5e62c7b9dc53b5e39763747702ee1e
Avaer Kazmer <a@modules.io>
2022-10-08 21:21:47 -0400

    Break out procgen utils

---
+++ public/utils/procgen-utils.js
@@ -0,0 +1,13 @@
+export const getScaleLod = scale => {
+  let scaleLod = Math.ceil(Math.log2(scale));
+  scaleLod = Math.max(scaleLod, 0);
+  scaleLod++;
+  return scaleLod;
+};
+export const getScaleInt = scale => {
+  const scaleLod = getScaleLod(scale);
+  // console.log('scale lod', scale, scaleLod);
+  // const scaleInt = Math.pow(2, scaleLod);
+  const scaleInt = 1 << (scaleLod - 1);
+  return scaleInt;
+};
