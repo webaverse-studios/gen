6ae63014b393ec89532d0fb2d78ce1129895c870
Avaer Kazmer <a@modules.io>
2022-10-08 23:26:57 -0400

    Add extra procgen util

--- public/utils/procgen-utils.js
+++ public/utils/procgen-utils.js
@@ -11,3 +11,11 @@ export const getScaleInt = scale => {
   const scaleInt = 1 << (scaleLod - 1);
   return scaleInt;
 };
+
+export const getBoundingSize = boundingType => {
+  switch (boundingType) {
+    case 'sphere': return 4;
+    case 'box': return 6;
+    default: return 0;
+  }
+};
