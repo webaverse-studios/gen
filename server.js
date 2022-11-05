f7f6a6d50b2256697851872b61fdad52aa721859
David Rowe <david@ctrlaltstudio.com>
2022-11-06 12:00:01 +1300

    Differentiate address and name

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import {parse} from 'url';
 import next from 'next';
 import fs from 'fs';
 
-const host = 'local.webaverse.com';
+const SERVER_ADDR = '0.0.0.0';
+const SERVER_NAME = 'local.webaverse.com';
 const port = parseInt(process.env.PORT, 10) || 4444;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({
@@ -25,9 +26,9 @@ app.prepare()
     createServer(httpsOptions, (req, res) => {
       const parsedUrl = parse(req.url, true);
       handle(req, res, parsedUrl);
-    }).listen(port, host, (err) => {
+    }).listen(port, SERVER_ADDR, (err) => {
       if (err) throw err;
-      console.log(`https://${host}:${port}/`);
+      console.log(`https://${SERVER_NAME}:${port}/`);
       console.log('ready');
     });
   }).catch(err => {
