b924996a7121d61b2ec381d75112cded5c9a1c4c
Avaer Kazmer <a@modules.io>
2022-11-07 17:35:47 -0500

    Checkpoint pipelines

--- pages/gen.js
+++ pages/gen.js
@@ -19,6 +19,7 @@ const prompts = {
   // map: `2D overhead view fantasy battle map scene, mysterious lush sakura forest, anime drawing, digital art`;
   map: `2D overhead view fantasy battle map scene, mysterious dinosaur robot factory, anime video game drawing, trending, winner, digital art`,
   world: `anime screenshot, mysterious forest path with japanese dojo doorway, neon arrows, jungle labyrinth, metal ramps, lush vegetation, ancient technology, mystery creature, glowing magic, ghibli style, digital art`,
+  character: `full body, cute young anime girl wearing a hoodie and skirt, white background, studio ghibli style, digital art`,
 };
 const labelClasses = ['person', 'floor', 'path', 'sidewalk', 'ground', 'road', 'runway', 'land', 'dirt', 'ceiling', 'field', 'river', 'water', 'sea', 'sky', 'mountain', 'leaves', 'wall', 'house', 'machine', 'rock', 'flower', 'door', 'gate', 'car', 'boat', 'animal', 'mat', 'grass', 'plant', 'metal', 'light', 'tree', 'wood', 'food', 'smoke', 'forest', 'shirt', 'pant', 'structure', 'bird', 'tunnel', 'cave', 'skyscraper', 'sign', 'stairs', 'box', 'sand', 'fruit', 'vegetable', 'barrier'];
 const groundBoost = 50;
@@ -1031,6 +1032,7 @@ const createSeedImage = (
   rh, // radius height
   p, // power distribution of radius
   n, // number of rectangles
+  shape, // 'ellipse' or 'rectangle'
   {
     color = null,
     monochrome = false,
@@ -1067,18 +1069,27 @@ const createSeedImage = (
     const sw = Math.pow(Math.random(), p) * rw;
     const sh = Math.pow(Math.random(), p) * rh;
     ctx.fillStyle = '#' + colors[Math.floor(Math.random() * colors.length)];
-    ctx.beginPath();
-    ctx.ellipse(x, y, sw, sh, 0, 0, 2 * Math.PI);
-    ctx.fill();
+    if (shape === 'ellipse') {
+      ctx.beginPath();
+      ctx.ellipse(x, y, sw, sh, 0, 0, 2 * Math.PI);
+      ctx.fill();
+    } else {
+      ctx.fillRect(x - sw / 2, y - sh / 2, sw, sh);
+    }
   }
 
   // make this box transparent in the mask
   // requires blend mode
   maskCtx.fillStyle = 'rgba(255, 255, 255, 1)';
   maskCtx.globalCompositeOperation = 'destination-out';
-  maskCtx.beginPath();
-  maskCtx.ellipse(w / 2, h / 2, w / 2 * 0.9, h / 2 * 0.9, 0, 0, 2 * Math.PI);
-  maskCtx.fill();
+  if (shape === 'ellipse') {
+    maskCtx.beginPath();
+    maskCtx.ellipse(w / 2, h / 2, w / 2 * 0.9, h / 2 * 0.9, 0, 0, 2 * Math.PI);
+    maskCtx.fill();
+  } else if (shape === 'rectangle') {
+    const innerRatio = 0.1;
+    maskCtx.fillRect(w * innerRatio, 0, w - w * innerRatio * 2, h);
+  }
 
   return {
     canvas,
@@ -2013,12 +2024,12 @@ if (typeof window !== 'undefined') {
     const file = files[0];
     if (file) {
       const u = URL.createObjectURL(file);
-      await globalThis.worldGen(u);
+      await globalThis.testWorldGen(u);
       URL.revokeObjectURL(u);
     }
   });
 }
-globalThis.worldGen = async image_url => {
+globalThis.testWorldGen = async image_url => {
   if (window.triggered) {
     // console.warn('already triggered', new Error().stack);
     // debugger;
@@ -2275,6 +2286,38 @@ globalThis.worldGen = async image_url => {
 
 //
 
+const editImg = async (blob, maskBlob, prompt) => {
+  const fd = getFormData({
+    image: blob,
+    mask: maskBlob,
+    prompt,
+    n: 1,
+    size: '1024x1024',
+  });
+  const response = await fetch(`https://api.openai.com/v1/images/edits`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${OPENAI_API_KEY}`,
+    },
+    body: fd,
+  });
+  const responseData = await response.json();
+  let image_url = responseData.data[0].url;
+
+  const u2 = new URL('/api/proxy', location.href);
+  u2.searchParams.set('url', image_url);
+  image_url = u2.href;
+
+  const img = new Image();
+  await new Promise((accept, reject) => {
+    img.onload = accept;
+    img.onerror = reject;
+    img.crossOrigin = 'Anonymous';
+    img.src = image_url;
+  });
+  // document.body.appendChild(img);
+  return img;
+};
 globalThis.testPotionSeed = async () => {
   const {
     canvas,
@@ -2285,7 +2328,8 @@ globalThis.testPotionSeed = async () => {
     64, // rw
     64, // rh
     1, // p
-    256 // n
+    256, // n
+    'ellipse',
   );
 
   canvas.classList.add('mainCanvas');
@@ -2308,6 +2352,143 @@ globalThis.testPotionSeed = async () => {
   const name = 'potion';
   const prompt = `video game item concept art render, trending on ArtStation, ${name}`;
 
+  const img = await editImg(blob, maskBlob, prompt);
+  document.body.appendChild(img);
+
+  return image_url;
+};
+
+//
+
+// spec is like [[0, 1], [2, 3]]
+const outpaintImage = async (img, prompt, specs) => {
+  const w = img.width;
+  const h = img.height;
+
+  // prepare global canvas
+  let canvasBounds = null;
+  for (let i = 0; i < specs.length; i++) {
+    const [dx, dy] = specs[i];
+    if (!canvasBounds) {
+      canvasBounds = [
+        dx * img.width,
+        dy * img.width,
+        dx * img.width + img.width,
+        dy * img.height + img.height,
+      ];
+    } else {
+      canvasBounds[0] = Math.min(canvasBounds[0], dx * img.width);
+      canvasBounds[1] = Math.min(canvasBounds[1], dy * img.height);
+      canvasBounds[2] = Math.max(canvasBounds[2], dx * img.width + img.width);
+      canvasBounds[3] = Math.max(canvasBounds[3], dy * img.height + img.height);
+    }
+  }
+  // create global canvas
+  const globalCanvas = document.createElement('canvas');
+  globalCanvas.width = canvasBounds[2] - canvasBounds[0];
+  globalCanvas.height = canvasBounds[3] - canvasBounds[1];
+  globalCanvas.classList.add('globalCanvas');
+  const globalCtx = globalCanvas.getContext('2d');
+  // draw the base image in the center
+  globalCtx.drawImage(img, -canvasBounds[0], -canvasBounds[1]);
+
+  // outpaint all specs in parallel
+  await Promise.all(specs.map(async (spec, i) => {
+    const [dx, dy] = spec;
+
+    const canvas = document.createElement('canvas');
+    if (!canvas.classList) {
+      debugger;
+    }
+    canvas.classList.add('canvas-' + i);
+    canvas.width = w;
+    canvas.height = h;
+    canvas.style.cssText = `\
+      background: red;
+    `;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#FFF';
+    ctx.fillRect(0, 0, w, h);
+    document.body.appendChild(canvas);
+
+    const maskCanvas = document.createElement('canvas');
+    if (!maskCanvas.classList) {
+      debugger;
+    }
+    maskCanvas.classList.add('maskCanvas-' + i);
+    maskCanvas.width = w;
+    maskCanvas.height = h;
+    maskCanvas.style.cssText = `\
+      background: red;
+    `;
+    const maskCtx = maskCanvas.getContext('2d');
+    maskCtx.fillStyle = '#FFF';
+    maskCtx.fillRect(0, 0, w, h);
+    document.body.appendChild(maskCanvas);
+
+    // draw the offsetted canvas
+    ctx.drawImage(img, -dx * w, -dy * h);
+    maskCtx.fillStyle = 'rgba(255, 255, 255, 1)';
+    maskCtx.globalCompositeOperation = 'destination-out';
+    maskCtx.fillRect(dx * w, dy * h, w, h);
+
+    const blob = await new Promise((accept, reject) => {
+      canvas.toBlob(accept, 'image/png');
+    });
+    const maskBlob = await new Promise((accept, reject) => {
+      maskCanvas.toBlob(accept, 'image/png');
+    });
+
+    // fetch from openai
+    const editedImg = await editImg(blob, maskBlob, prompt);
+    // console.log('got edited img', editedImg);
+    if (!editedImg.classList) {
+      debugger;
+    }
+    editedImg.classList.add('editImg-' + i);
+    document.body.appendChild(editedImg);
+
+    // draw back to the global canvas
+    globalCtx.drawImage(editedImg, dx * w - canvasBounds[0], dy * h - canvasBounds[1]);
+  }));
+
+  // console.log('return global canvas', globalCanvas);
+
+  return globalCanvas;
+};
+globalThis.testCharacterSeed = async () => {
+  /* const {
+    canvas,
+    maskCanvas,
+  } = createSeedImage(
+    512, // w
+    512, // h
+    128, // rw
+    128, // rh
+    1, // p
+    256, // n
+    'rectangle',
+  );
+
+  canvas.classList.add('mainCanvas');
+  canvas.style.cssText = `\
+    background: red;
+  `;
+  document.body.appendChild(canvas);
+  maskCanvas.classList.add('maskCanvas');
+  maskCanvas.style.cssText = `\
+    background: red;
+  `;
+  document.body.appendChild(maskCanvas);
+
+  const blob = await new Promise((accept, reject) => {
+    canvas.toBlob(accept, 'image/png');
+  });
+  const maskBlob = await new Promise((accept, reject) => {
+    maskCanvas.toBlob(accept, 'image/png');
+  });
+  const prompt = prompts.character;
+
   const fd = getFormData({
     image: blob,
     mask: maskBlob,
@@ -2329,6 +2510,18 @@ globalThis.testPotionSeed = async () => {
 
   const u2 = new URL('/api/proxy', location.href);
   u2.searchParams.set('url', image_url);
+  image_url = u2.href; */
+
+  const prompt = prompts.character;
+  const response = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  let image_url = response.data.data[0].url;
+
+  const u2 = new URL('/api/proxy', location.href);
+  u2.searchParams.set('url', image_url);
   image_url = u2.href;
 
   const img = new Image();
@@ -2340,7 +2533,12 @@ globalThis.testPotionSeed = async () => {
   });
   document.body.appendChild(img);
 
-  return image_url;
+  const outpaintedCanvas = await outpaintImage(img, prompt, [
+    [0, -0.5],
+    [0, 0.5],
+  ]);
+  console.log('done outpainting', outpaintedCanvas);
+  document.body.appendChild(outpaintedCanvas);
 };
 
 //
