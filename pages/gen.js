dd594705b1a6bce3caf1db14e2ec967e17e34443
Avaer Kazmer <a@modules.io>
2022-11-09 22:03:07 -0500

    Dead code cleanup

--- pages/gen.js
+++ pages/gen.js
@@ -9,32 +9,12 @@ import styles from '../styles/Gen.module.css';
 
 //
 
-const vqaQueries = [
+/* const vqaQueries = [
   `is this birds eye view?`,
   `is the viewer looking up at the sky?`,
   `is the viewer looking up at the ceiling?`,
   `how many feet tall is the viewer?`,
-];
-
-//
-
-if (typeof window !== 'undefined') {
-  document.addEventListener('dragover', e => {
-    e.preventDefault();
-    e.stopPropagation();
-  });
-  document.addEventListener('drop', async e => {
-    e.preventDefault();
-    e.stopPropagation();
-    const files = e.dataTransfer.files;
-    const file = files[0];
-    if (file) {
-      // const u = URL.createObjectURL(file);
-      await sceneGenerator.generate(file);
-      // URL.revokeObjectURL(u);
-    }
-  });
-}
+]; */
 
 //
 
