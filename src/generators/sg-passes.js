d3a9d00e51f3cf0317bc61c8af29dcee8fce7465
Avaer Kazmer <a@modules.io>
2022-12-03 19:03:17 -0500

    Major floor net mesh rendering improvement

--- src/generators/sg-passes.js
+++ src/generators/sg-passes.js
@@ -32,12 +32,9 @@ import {
 
 //
 
-// const localVector = new THREE.Vector3();
+const localVector = new THREE.Vector3();
 const localQuaternion = new THREE.Quaternion();
-
-/* const fakeMaterial = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-}); */
+const localRay = new THREE.Ray();
 
 //
 
@@ -63,7 +60,8 @@ export function reconstructFloor({
 
   const floorNetCameraJson = getOrthographicCameraJson(floorNetCamera);
 
-
+  const floorPlane2 = floorPlane.clone();
+  // floorPlane2.normal.negate();
 
 
 
@@ -75,7 +73,66 @@ export function reconstructFloor({
   const width = floorNetPixelSize;
   const height = floorNetPixelSize;
   const meshes = getRenderSpecsMeshes(renderSpecs, floorNetCamera);
-  const floorNetDepths = getRenderSpecsMeshesDepth(meshes, width, height, floorNetCamera);
+
+  const floorNetDepthsOriginal = getRenderSpecsMeshesDepth(meshes, width, height, floorNetCamera);
+  globalThis.floorNetDepthsOriginal = floorNetDepthsOriginal;
+  // globalThis.hits = [];
+  const floorNetDepths = new Float32Array(floorNetDepthsOriginal.length);
+  globalThis.floorNetDepths = floorNetDepths;
+  const offset = 0.1;
+  for (let i = 0; i < floorNetDepthsOriginal.length; i++) {
+    let value = floorNetDepthsOriginal[i];
+    if (value !== 0) {
+      value = value - offset;
+    } else {
+      // sample around the pixel to see if there's a nearby depth
+      const range = 3;
+      let sum = 0;
+      let total = 0;
+      for (let dy = -range; dy <= range; dy++) {
+        for (let dx = -range; dx <= range; dx++) {
+          // if (dx === 0 && dy === 0) {
+          //   continue;
+          // }
+
+          const _planeHit = () => {
+            localRay.origin.copy(floorNetCamera.position);
+            localRay.origin.x = (x / width - 0.5) * floorNetWorldSize;
+            localRay.origin.z = (y / height - 0.5) * floorNetWorldSize;
+            localRay.direction.set(0, 0, -1)
+              .applyQuaternion(floorNetCamera.quaternion);
+            const point = localRay.intersectPlane(floorPlane2, localVector);
+            sum += -(point.y - floorNetCamera.position.y) * weight;
+            // if (hits.length < 1024) {
+            //   hits.push(value);
+            // }
+          };
+
+          const x = i % width + dx;
+          const y = Math.floor(i / width) + dy;
+          const index = y * width + x;
+          const distance = Math.sqrt(dx*dx + dy*dy);
+          const weight = 1 / (1 + distance);
+          if (x >= 0 && x < width && y >= 0 && y < height) {
+            const value2 = floorNetDepthsOriginal[index];
+            if (value2 !== 0) { // real hit
+              sum += value2 * weight;
+            } else { // fake plane hit
+              _planeHit();
+            }
+          } else {
+            _planeHit();
+          }
+          total += weight;
+        }
+      }
+      if (total !== 0) {
+        sum /= total;
+      }
+      value = sum;
+    }
+    floorNetDepths[i] = value;
+  }
 
 
 
