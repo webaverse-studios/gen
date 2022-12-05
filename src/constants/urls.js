f6552b28cab1b41819a7c65717c412be166b616b
Avaer Kazmer <a@modules.io>
2022-12-05 10:37:22 -0500

    Add missing urls.js

---
+++ src/constants/urls.js
@@ -0,0 +1,8 @@
+export const mobUrls = [
+  'silkworm.glb',
+  'silkworm-biter.glb',
+  'silkworm-bloater.glb',
+  // 'silkworm-queen.glb',
+  'silkworm-runner.glb',
+  'silkworm-slasher.glb',
+].map(name => `./models/Mob_Bases/${name}`);
