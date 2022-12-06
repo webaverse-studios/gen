bcca1faba111dc9a95aba7a8ab37c9974a40812d
Avaer Kazmer <a@modules.io>
2022-12-06 18:05:42 -0500

    Avatar generator checkpoint

--- src/components/generators/AvatarGeneratorComponent.jsx
+++ src/components/generators/AvatarGeneratorComponent.jsx
@@ -53,15 +53,16 @@ const size = 1024;
 
 //
 
-const generateAvatar = async (canvas, prompt) => {
+const generateAvatar = async (canvas, prompt, negativePrompt) => {
   const renderer = makeRenderer(canvas);
 
   const scene = new THREE.Scene();
   scene.autoUpdate = false;
 
   const camera = makeDefaultCamera();
-  camera.position.set(0, 1, -4);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  camera.position.set(0, 0.9, -2);
+  // camera.lookAt(new THREE.Vector3(0, camera.position.y, 0));
+  camera.quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
   camera.updateMatrixWorld();
 
   const light = new THREE.DirectionalLight(0xffffff, 2);
@@ -72,12 +73,16 @@ const generateAvatar = async (canvas, prompt) => {
   const ambientLight = new THREE.AmbientLight(0xffffff, 1);
   scene.add(ambientLight);
 
+  const localColor = new THREE.Color();
+
   const controls = new OrbitControls(camera, canvas);
   controls.minDistance = 1;
   controls.maxDistance = 100;
   const targetDistance = -camera.position.z;
   controls.target.copy(camera.position)
-    .addScaledVector(camera.getWorldDirection(new THREE.Vector3()), targetDistance);
+    // .addScaledVector(new THREE.Vector3(0, 0, -1).applyQuaternion(camera.quaternion), targetDistance);
+  controls.target.z = 0;
+  controls.update();
 
   const avatars = new THREE.Object3D();
   (async () => {
@@ -93,23 +98,16 @@ const generateAvatar = async (canvas, prompt) => {
     model = model.scene;
 
     // recompile the model
-    const getMeshSpecs = model => {
+    const getMeshes = model => {
       const meshes = [];
-      const materials = [];
       model.traverse(o => {
         if (o.isMesh) {
           meshes.push(o);
-          materials.push(o.material);
         }
       });
-      return {
-        meshes,
-        materials,
-      };
+      return meshes;
     };
-    let meshSpecs = getMeshSpecs(model);
-    let meshes = meshSpecs.meshes;
-    let materials = meshSpecs.materials;
+    let meshes = getMeshes(model);
 
     // sort meshes into invisible categories
     const categorySpecs = [
@@ -162,7 +160,8 @@ const generateAvatar = async (canvas, prompt) => {
           };
           categories[categorySpec.name] = entry;
         }
-        mesh.className = categorySpec.className;
+        // mesh.className = categorySpec.className;
+        mesh.className = 'avatar';
         entry.meshes.push(mesh);
       } else {
         console.warn('failed to match mesh to category', name);
@@ -176,7 +175,9 @@ const generateAvatar = async (canvas, prompt) => {
     }
 
     // select and show a random mesh from each category
-    const rng = alea('lol');
+    const seed = 'lol' + Math.random();
+    globalThis.seed = seed;
+    const rng = alea(seed);
     const categorySelections = {};
     for (const categorySpec of categorySpecs) {
       const {name} = categorySpec;
@@ -217,50 +218,182 @@ const generateAvatar = async (canvas, prompt) => {
     avatars.add(model);
     model.updateMatrixWorld();
 
-    // latch the new mesh specs
-    meshSpecs = getMeshSpecs(model);
-    meshes = meshSpecs.meshes;
-    materials = meshSpecs.materials;
+    {
+      const canvas = document.createElement('canvas');
+      canvas.classList.add('frontCanvas');
+      canvas.width = size;
+      canvas.height = size;
+      canvas.style.cssText = `\
+        background: red;
+      `;
+      document.body.appendChild(canvas);
 
-    // XXX rerender the textures with AI
-    for (let i = 0; i < materials.length; i++) {
-      // const material = materials[i];
-      // console.log('got material', material);
-      // const {map} = material;
-      // const {image} = map;
-      // image.style.cssText = `\
-      //   position: relative;
-      //   width: 1024px;
-      //   height: 1024px;
-      // `;
-      // document.body.appendChild(image);
+      const renderer2 = makeRenderer(canvas);
 
-      // // resize to size
-      // const canvas = document.createElement('canvas');
-      // canvas.width = size;
-      // canvas.height = size;
-      // const ctx = canvas.getContext('2d');
-      // ctx.drawImage(image, 0, 0, size, size);
-      
-      // const blob = await canvas2blob(canvas);
-      const opaqueImgDataUrl = await image2DataUrl(canvas);
+      const scene2 = new THREE.Scene();
+      scene2.autoUpdate = false;
 
-      // const editImg = await img2img({
-      //   prompt,
-      //   width: image.width,
-      //   height: image.height,
-      //   imageDataUrl: opaqueImgDataUrl,
-      //   // imageDataUrl: maskImgDataUrl,
-      //   maskImageDataUrl: maskImgDataUrl,
-      // });
-  
-      // generateTextureMaps
+      const camera2 = new THREE.OrthographicCamera(
+        -1, // left
+        1, // right
+        1, // top
+        -1, // bottom
+        0, // near
+        1000 // far
+      );
+      camera2.position.copy(camera.position);
+      camera2.quaternion.copy(camera.quaternion);
+      camera2.updateMatrixWorld();
+
+      const popMeshes = pushMeshes(scene2, [
+        model,
+      ]);
+
+      renderer2.render(scene2, camera2);
+
+      popMeshes();
+
+      // post process
+      const processingCanvas = document.createElement('canvas');
+      processingCanvas.width = size;
+      processingCanvas.height = size;
+      const processingContext = processingCanvas.getContext('2d');
+      processingContext.drawImage(renderer2.domElement, 0, 0);
+      const opaqueImageData = processingContext.getImageData(0, 0, size, size);
+      const maskImageData = processingContext.getImageData(0, 0, size, size);
+
+      const candidateColors = colors.slice();
+
+      // const backgroundColor = 0xFFFFFF;
+      const backgroundColor = 0x000000;
+      // const backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+      // const backgroundColor = candidateColors.splice(Math.floor(Math.random() * candidateColors.length), 1)[0];
+      const bgColor = new THREE.Color(backgroundColor);
+
+      // const backgroundColor2 = 0xFFFFFF;
+      const backgroundColor2 = 0x000000;
+      // const backgroundColor2 = colors[Math.floor(Math.random() * colors.length)];
+      // const backgroundColor2 = candidateColors.splice(Math.floor(Math.random() * candidateColors.length), 1)[0];
+      // const backgroundColor2 = backgroundColor;
+      const bgColor2 = new THREE.Color(backgroundColor2);
+
+      const maskBgAlpha = 0;
+      const maskFgAlpha = 0.07;
+      const opaqueBgAlpha = 0.5;
+      const opaqueFgAlpha = 0.5;
+
+      for (let i = 0; i < opaqueImageData.data.length; i += 4) {
+        if (opaqueImageData.data[i + 3] < 255) { // transparent, background
+          // localColor.setHex(0xFFFFFF).toArray(opaqueImageData.data, i);
+          opaqueImageData.data[i + 0] = bgColor.r * 255;
+          opaqueImageData.data[i + 1] = bgColor.g * 255;
+          opaqueImageData.data[i + 2] = bgColor.b * 255;
+          opaqueImageData.data[i + 3] = opaqueBgAlpha * 255;
+          maskImageData.data[i + 3] = maskBgAlpha * 255;
+        } else { // opaque, foreground
+          opaqueImageData.data[i + 3] = opaqueFgAlpha * 255;
+          maskImageData.data[i + 3] = maskFgAlpha * 255;
+        }
+      }
+
+      // draw the canvases for debugging
+      const opaqueCanvas = document.createElement('canvas');
+      opaqueCanvas.width = size;
+      opaqueCanvas.height = size;
+      opaqueCanvas.style.cssText = `\
+        background: red;
+      `;
+      document.body.appendChild(opaqueCanvas);
+      const opaqueContext = opaqueCanvas.getContext('2d');
+      opaqueContext.putImageData(opaqueImageData, 0, 0);
+
+      const maskCanvas = document.createElement('canvas');
+      maskCanvas.width = size;
+      maskCanvas.height = size;
+      maskCanvas.style.cssText = `\
+        background: red;
+      `;
+      document.body.appendChild(maskCanvas);
+      const maskContext = maskCanvas.getContext('2d');
+      maskContext.putImageData(maskImageData, 0, 0);
+
+      const [
+        opaqueImgDataUrl,
+        maskImgDataUrl,
+      ] = await Promise.all([
+        image2DataUrl(opaqueCanvas),
+        image2DataUrl(maskCanvas),
+      ]);
+
+      const editImg = await img2img({
+        prompt,
+        negativePrompt,
+        width: size,
+        height: size,
+        imageDataUrl: opaqueImgDataUrl,
+        // imageDataUrl: maskImgDataUrl,
+        maskImageDataUrl: maskImgDataUrl,
+      });
+      console.log('edit image', editImg);
+      document.body.appendChild(editImg);
     }
 
-    globalThis.model = model;
-    globalThis.meshes = meshes;
-    globalThis.materials = materials;
-    // globalThis.categories = categories;
+    /* // latch the new mesh specs
+    meshes = getMeshes(model);
+    // if (meshes.length !== 1) {
+    //   console.warn('only one mesh is supported', meshes.length);
+    // }
+    console.log('new meshes', meshes);
+    globalThis.meshes = meshes.slice();
+
+    for (const mesh of meshes) {
+      mesh.visible = false;
+    }
+
+    // XXX
+    let first = true;
+    for (let i = 0; i < meshes.length; i++) {
+      const mesh = meshes[i];
+      mesh.visible = true;
+      const {
+        width,
+        height,
+        maskImgDataUrl,
+        opaqueImgDataUrl,
+      } = await preprocessMeshForTextureEdit(mesh, {
+        textureSize: size,
+        flipY: true,
+      });
+      
+      console.log('ok 1', {
+        width,
+        height,
+        maskImgDataUrl,
+        opaqueImgDataUrl,
+      });
+
+      const mesh2 = await editMeshTextures(mesh, {
+        prompt,
+        width,
+        height,
+        opaqueImgDataUrl,
+        maskImgDataUrl,
+        flipY: false,
+      });
+
+      console.log('ok 2', mesh2);
+
+      if (first) {
+        first = false;
+        const parent = mesh.parent;
+        parent.remove(mesh);
+        parent.add(mesh2);
+      }
+    } */
+
+    // globalThis.model = model;
+    // globalThis.mesh = mesh;
+    // globalThis.mesh2 = mesh2;
   })();
   scene.add(avatars);
 
@@ -272,7 +405,8 @@ const generateAvatar = async (canvas, prompt) => {
   _render();
 };
 
-const defaultPrompt = 'diffuse texture, Unreal Engine anime video game, JRPG character VRChat VRoid Hub Pixiv';
+const defaultPrompt = 'anime style, girl character, 3d model orthographic view, cute pink dress';
+const negativePrompt = 'side, back';
 const AvatarGeneratorComponent = () => {
   const [prompt, setPrompt] = useState(defaultPrompt);
   const [generated, setGenerated] = useState(false);
@@ -283,7 +417,7 @@ const AvatarGeneratorComponent = () => {
     const canvas = canvasRef.current;
     if (canvas && !generated) {
       setGenerated(true);
-      await generateAvatar(canvas, prompt);
+      await generateAvatar(canvas, prompt, negativePrompt);
     }
   };
 
