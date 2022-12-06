5a91b2d9523532fa1deffc4d5a0b011e70313cb3
Avaer Kazmer <a@modules.io>
2022-12-06 08:11:12 -0500

    Abstraction cleanup

--- src/components/generators/MobGeneratorComponent.jsx
+++ src/components/generators/MobGeneratorComponent.jsx
@@ -10,6 +10,9 @@ import {
   txt2img,
   img2img,
 } from '../../clients/sd-image-client.js';
+import {
+  generateTextureMaps,
+} from '../../clients/material-map-client.js';
 import {mobUrls} from '../../constants/urls.js';
 import {
   makeRenderer,
@@ -34,46 +37,6 @@ import {
   canvas2blob,
   img2canvas,
 } from '../../utils/convert-utils.js';
-
-//
-
-const generateTextureMaps = async img => {
-  // if (!img) {
-  //   debugger;
-  //   img = await loadImage('/images/fluffy.png');
-  // }
-  const canvas = img2canvas(img);
-  const blob = await canvas2blob(canvas);
-  const [
-    normalImage,
-    roughnessImage,
-    displacementImage,
-  ] = await Promise.all([
-    'n',
-    'r',
-    'd',
-  ].map(async shortname => {
-    const res = await fetch(`https://stable-diffusion.webaverse.com/material?mode=seamless&map=${shortname}`, {
-      method: 'post',
-      body: blob,
-    });
-    if (res.ok) {
-      const img2Blob = await res.blob();
-      const img2 = await blob2img(img2Blob);
-      // console.log('got response', img2, URL.createObjectURL(img2Blob));
-      document.body.appendChild(img2);
-      return img2;
-    } else {
-      throw new Error('invalid status: ' + res.status);
-    }
-  }));
-  return {
-    normalImage,
-    roughnessImage,
-    displacementImage,
-  };
-};
-globalThis.generateTextureMaps = generateTextureMaps;
 
 //
 
