c49a718bbded2723a93c78ff854f4fd97396fb82
Avaer Kazmer <a@modules.io>
2022-12-05 23:07:18 -0500

    Update seedImage function

--- canvas/seed-image.js
+++ canvas/seed-image.js
@@ -1,14 +1,17 @@
+import * as THREE from 'three';
 import {createCanvas, loadImage} from 'canvas';
-import materialColors from './material-colors.json';
-import ColorScheme from './color-scheme.js';
+// import materialColors from './material-colors.json';
+// import ColorScheme from './color-scheme.js';
+import {colors} from '../src/constants/detectron-colors.js';
 
 //
 
-const baseColors = Object.keys(materialColors).map(k => materialColors[k][400].slice(1));
+// const baseColors = Object.keys(materialColors).map(k => materialColors[k][400].slice(1));
 
 //
 
 const rng = () => (Math.random() * 2) - 1;
+const localColor = new THREE.Color();
 
 //
 
@@ -35,12 +38,20 @@ export const createSeedImage = (
   ctx.fillRect(0, 0, w, h);
   // ctx.filter = blur ? `blur(${blur}px) saturate(1.5)` : '';
   
-  const baseColor = color ?? baseColors[Math.floor(Math.random() * baseColors.length)];
-  const scheme = new ColorScheme();
-  scheme.from_hex(baseColor)
-    .scheme(monochrome ? 'mono' : 'triade')   
-    // .variation('hard');
-  const colors = scheme.colors();
+  // const baseColor = color ?? baseColors[Math.floor(Math.random() * baseColors.length)];
+  // const scheme = new ColorScheme();
+  // scheme.from_hex(baseColor)
+  //   .scheme(monochrome ? 'mono' : 'triade')   
+  //   // .variation('hard');
+  // const colors = scheme.colors();
+  const cs = [];
+  const candidateColors = colors.slice();
+  for (let i = 0; i < 6; i++) {
+    const color = candidateColors.splice(Math.floor(Math.random() * candidateColors.length), 1)[0];
+    localColor.setHex(color)
+      .offsetHSL(0, -0.2, 0);
+    cs.push(localColor.getHexString());
+  }
 
   for (let i = 0; i < n; i++) {
     const x = w / 2 + rng() * rw;
@@ -48,7 +59,7 @@ export const createSeedImage = (
     const sw = Math.pow(Math.random(), p) * rw;
     const sh = Math.pow(Math.random(), p) * rh;
     // ctx.fillStyle = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-    ctx.fillStyle = '#' + colors[Math.floor(Math.random() * colors.length)];
+    ctx.fillStyle = '#' + cs[Math.floor(Math.random() * cs.length)];
     // ctx.fillStyle = '#' + baseColors[Math.floor(Math.random() * baseColors.length)];
 
     ctx.fillRect(x - sw / 2, y - sh / 2, sw, sh);
