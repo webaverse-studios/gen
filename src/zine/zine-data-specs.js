63cf877e4a460ad8b5c1421de46f99de41ac160a
Avaer Kazmer <a@modules.io>
2022-12-07 17:15:22 -0500

    Add missing zine src

---
+++ src/zine/zine-data-specs.js
@@ -0,0 +1,47 @@
+export const mainImageKey = 'image';
+export const promptKey = 'prompt';
+export const layer0Specs = [
+  mainImageKey,
+  promptKey,
+];
+
+//
+
+export const layer1Specs = [
+  'segmentMask',
+  'pointCloudHeaders',
+  'pointCloud',
+  'planesJson',
+  'planesMask',
+  'portalJson',
+  'segmentSpecs',
+  'planeSpecs',
+  'portalSpecs',
+  'firstFloorPlaneIndex',
+  'floorPlaneJson',
+  'floorNetDepths',
+  'floorNetCameraJson',
+  'predictedHeight',
+  'portalLocations',
+];
+
+//
+
+export const layer2Specs = [
+  'maskImg',
+  'editedImg',
+  'pointCloudHeaders',
+  'pointCloud',
+  'depthFloatImageData',
+  'distanceFloatImageData',
+  'distanceNearestPositions',
+  'newDepthFloatImageData',
+  'reconstructedDepthFloats',
+  'planesJson',
+  'planesMask',
+  'portalJson',
+  'floorNetDepths',
+  'floorNetCameraJson',
+  'segmentMask',
+  'editCameraJson',
+];
