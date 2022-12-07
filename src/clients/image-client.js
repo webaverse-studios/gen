5c2e7fcd6ee8039da8e2424cb5682e91d25b611a
Avaer Kazmer <a@modules.io>
2022-12-07 07:07:50 -0500

    Add DALLE img2img api

--- src/clients/image-client.js
+++ src/clients/image-client.js
@@ -187,3 +187,32 @@ export class ImageAiServer {
     }
   }
 }
+
+export const img2img = async ({
+  prompt = 'test',
+  // negativePrompt = '',
+  // width = 512,
+  // height = 512,
+  blob,
+  maskBlob,
+  // maskBlur = 4, // default 4
+  // maskTransparency = 0,
+  // falloffExponent = 1, // default 1
+  // randomness = 0, // default 0
+} = {}) => {
+  const n = 1;
+
+  const fd = makeEditImgFormData(blob, maskBlob, prompt);
+  const u = new URL('/api/ai/image-ai/editImgBlob', location.href);
+  u.searchParams.set('prompt', prompt);
+  if (n !== undefined) {
+    u.searchParams.set('n', n);
+  }
+  const res = await fetch(u, {
+    method: 'POST',
+    body: fd,
+  });
+  const resultBlob = await res.blob();
+  const img = await blob2img(resultBlob);
+  return img;
+};
