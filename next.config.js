f9bd60778455d32ddc899525f8f9aef1e95e8e8e
Avaer Kazmer <a@modules.io>
2022-12-10 21:18:45 -0500

    Config cleanup

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
-
 const securityHeaders = [
   {
     key: 'Cross-Origin-Opener-Policy',
@@ -28,7 +17,7 @@ const securityHeaders = [
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   async headers() {
     return [
       {
