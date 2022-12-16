e979e646e30c2e0162d83b18b18e9a9d23a0b485
Avaer Kazmer <a@modules.io>
2022-12-16 14:49:32 -0500

    Hook in new item generator jsx

--- src/components/generators/ItemGeneratorComponent.jsx
+++ src/components/generators/ItemGeneratorComponent.jsx
@@ -4,6 +4,7 @@ import {prompts} from '../../constants/prompts.js';
 import {ItemGenerator} from '../../generators/item-generator.js';
 
 import styles from '../../../styles/Gen.module.css';
+import {downloadFile} from '../../utils/http-utils.js';
 
 //
 
@@ -11,26 +12,32 @@ const itemGenerator = new ItemGenerator();
 
 //
 
-const vqaQueries = [
-  `is this birds eye view?`,
-  `is the viewer looking up at the sky?`,
-  `is the viewer looking up at the ceiling?`,
-  `how many feet tall is the viewer?`,
-];
-
-//
-
 const ItemGeneratorComponent = () => {
   const [prompt, setPrompt] = useState(prompts.item);
-  
+  const [itemInstance, setItemInstance] = useState(null);
+
   return (
     <div className={styles.generator}>
       <input type="text" className={styles.input} value={prompt} onChange={e => {
         setPrompt(e.target.value);
       }} placeholder={prompts.item} />
       <div className={styles.button} onClick={async () => {
-        await itemGenerator.generate(prompt); // XXX split this into generate and render
+        const newItemInstance = await itemGenerator.generate(prompt);
+        setItemInstance(newItemInstance);
       }}>Generate</div>
+      {itemInstance ? <>
+        <div className={styles.button} onClick={async () => {
+          // download the images
+          const {imgBlob} = itemInstance;
+          downloadFile(imgBlob, 'item.png');
+        }}>DL images</div>
+        <div className={styles.button} onClick={async () => {
+          // download the images
+          const {glbBlob} = itemInstance;
+          downloadFile(glbBlob, 'item.glb');
+        }}>DL model</div>
+      </>
+      : null}
     </div>
   );
 };
