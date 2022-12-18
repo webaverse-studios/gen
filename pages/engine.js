5fc4e5d75566816877711fee9bce68258b816e4e
Avaer Kazmer <a@modules.io>
2022-12-18 02:31:31 -0500

    Offscreen engine api use ZineStoryboard multi-panel export

--- pages/engine.js
+++ pages/engine.js
@@ -2,13 +2,37 @@ import offscreenEngineApi from 'offscreen-engine/offscreen-engine-api.js';
 import {
   compileVirtualScene,
 } from '../src/generators/scene-generator.js';
+import {
+  ZineStoryboard,
+} from '../src/zine/zine-format.js';
 import {zbencode} from '../src/zine/encoding.js';
+import {
+  mainImageKey,
+  promptKey,
+  layer0Specs,
+  layer1Specs,
+} from '../src/zine/zine-data-specs.js';
 
 offscreenEngineApi(async (funcName, args, opts) => {
   if (funcName === 'compileScene') {
-    const {imageArrayBuffer} = args;
-    const storyboardObject = await compileVirtualScene(imageArrayBuffer);
-    const uint8Array = zbencode(storyboardObject);
+    const {imageArrayBuffer, prompt = ''} = args;
+
+    const storyboard = new ZineStoryboard();
+    const panel0 = storyboard.addPanel();
+
+    const layer0 = panel0.addLayer();
+    layer0.setData(mainImageKey, imageArrayBuffer);
+    layer0.setData(promptKey, prompt);
+
+    const compileResult = await compileVirtualScene(imageArrayBuffer);
+
+    const layer1 = panel0.addLayer();
+    for (const name of layer1Specs) {
+      const v = compileResult[name];
+      layer1.setData(name, v);
+    }
+    
+    const uint8Array = storyboard.export();
     return uint8Array;
   } else {
     throw new Error('unknown function: ' + funcName);
