cad375372ad97c606022d08a05429fa9b7d21536
Avaer Kazmer <a@modules.io>
2022-12-18 02:02:04 -0500

    Return uint8Array from offscreen engine compileScene api

--- pages/engine.js
+++ pages/engine.js
@@ -2,12 +2,14 @@ import offscreenEngineApi from 'offscreen-engine/offscreen-engine-api.js';
 import {
   compileVirtualScene,
 } from '../src/generators/scene-generator.js';
+import {zbencode} from '../src/zine/encoding.js';
 
 offscreenEngineApi(async (funcName, args, opts) => {
   if (funcName === 'compileScene') {
     const {imageArrayBuffer} = args;
-    const result = await compileVirtualScene(imageArrayBuffer);
-    return result;
+    const storyboardObject = await compileVirtualScene(imageArrayBuffer);
+    const uint8Array = zbencode(storyboardObject);
+    return uint8Array;
   } else {
     throw new Error('unknown function: ' + funcName);
   }
