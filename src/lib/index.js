5d3baf60ba2b36d1f8e805cb6905e33b0cabe801
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2022-12-22 01:29:06 -0800

    Remove lib directory

--- src/lib/index.js
+++
@@ -1,5 +0,0 @@
-
-export { compressBlob } from './compressBlob.js';
-export { decompressBlob } from './decompressBlob.js';
-export { pipeResponseToFile } from './pipeResponseToFile.js';
-export { saveCompressedBlob } from './saveCompressedBlob.js';
