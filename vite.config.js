eeb170aeb9d5c37494be10d9e52ce3d5a30a6238
Avaer Kazmer <a@modules.io>
2022-12-31 01:16:04 -0500

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import {defineConfig} from 'vite'
+// import {swcReactRefresh} from "vite-plugin-swc-react-refresh";
+import pluginSwc from '@vitejs/plugin-react-swc';
+// import metaversefilePlugin from 'metaversefile/plugins/rollup.js'
+
+// https://vitejs.dev/config/
+export default defineConfig(({command, mode, ssrBuild}) => {
+  return {
+    plugins: [
+      pluginSwc({
+        // fastRefresh: false,
+      }),
+    ],
+    build: {
+      minify: process.env.NODE_ENV === 'production',
+    },
+    worker:{
+      format: 'esm',
+    },
+    /* optimizeDeps: {
+      entries: [
+        'src/*.js',
+        'src/*.jsx',
+        'avatars/*.js',
+        'avatars/vrarmik/*.js',
+        'src/components/*.js',
+        'src/components/*.jsx',
+        'src/tabs/*.jsx',
+        '*.js'
+      ],
+    }, */
+    server: {
+      fs: {
+        strict: true,
+      },
+      host: '0.0.0.0',
+      port: 9999,
+    },
+  };
+})
