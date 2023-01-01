f4f7f5309cdeaab960cebd09808300c3aa9d5173
Avaer Kazmer <a@modules.io>
2023-01-01 07:07:06 -0500

    Shaders cleanup

--- src/components/generators/MetasceneGeneratorComponent.jsx
+++ src/components/generators/MetasceneGeneratorComponent.jsx
@@ -248,18 +248,18 @@ class SceneBatchedMesh extends THREE.Mesh {
       },
       vertexShader: `\
         varying vec2 vUv;
-        varying vec3 vNormal;
+        // varying vec3 vNormal;
 
         void main() {
           vUv = uv;
-          vNormal = normal;
+          // vNormal = normal;
           gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         }
       `,
       fragmentShader: `\
         uniform sampler2D map;
         varying vec2 vUv;
-        varying vec3 vNormal;
+        // varying vec3 vNormal;
 
         void main() {
           // gl_FragColor = vec4(vNormal, 1.0);
