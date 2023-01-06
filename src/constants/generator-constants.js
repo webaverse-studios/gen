b9b92135ef0fc4f895b410e4457f1c44f647a6eb
Avaer Kazmer <a@modules.io>
2023-01-06 07:09:57 -0500

    Add dev server constants in generator-constants.js

--- src/constants/generator-constants.js
+++ src/constants/generator-constants.js
@@ -4,3 +4,6 @@ export const targetScaleInv = 1 / targetScale;
 
 // for world meshes
 export const targetWorldScale = 0.2;
+
+export const devServerUrl = `https://local.webaverse.com`;
+export const devServerTmpUrl = `${devServerUrl}/tmp`;
