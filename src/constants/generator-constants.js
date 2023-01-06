cb4fc63b860eacc80491e0127e9319182b48269c
Avaer Kazmer <a@modules.io>
2023-01-06 07:10:31 -0500

    Constants commenting

--- src/constants/generator-constants.js
+++ src/constants/generator-constants.js
@@ -5,5 +5,6 @@ export const targetScaleInv = 1 / targetScale;
 // for world meshes
 export const targetWorldScale = 0.2;
 
+// for passing off to the dev server
 export const devServerUrl = `https://local.webaverse.com`;
 export const devServerTmpUrl = `${devServerUrl}/tmp`;
