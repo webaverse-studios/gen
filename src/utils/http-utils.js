966adad6d633e59e692ea565e22ada4a20d79293
Avaer Kazmer <a@modules.io>
2023-01-06 07:10:53 -0500

    Add openZineFile method

--- src/utils/http-utils.js
+++ src/utils/http-utils.js
@@ -1,3 +1,7 @@
+import {
+  devServerTmpUrl,
+} from '../constants/generator-constants.js';
+
 export const getFormData = o => {
   const formData = new FormData();
   for (const k in o) {
@@ -16,4 +20,25 @@ export function downloadFile(file, filename) {
   document.body.appendChild(tempLink);
   tempLink.click();
   document.body.removeChild(tempLink);
+}
+
+let ids = 0;
+export async function openZineFile(file) {
+  const u = `${devServerTmpUrl}/zine-${++ids}.zine`;
+  const res = await fetch(u, {
+    method: 'PUT',
+    body: file,
+  });
+  // console.log('got res', u, res);
+  const blob2 = await res.blob();
+  // console.log('got result', u, blob2);
+
+  // const devServerUrl 
+  const u2 = new URL(`https://local.webaverse.com/`);
+  u2.searchParams.set('src', u);
+
+  // console.log('got u', u2.href, blob2);
+
+  // open in new tab
+  window.open(u2.href, '_blank');
 }
