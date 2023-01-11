3aafd5114e808604d1ce6aa547dd5b07ea5f7850
Avaer Kazmer <a@modules.io>
2023-01-11 14:14:28 -0500

    Add missing memory-utils.js

---
+++ src/utils/memory-utils.js
@@ -0,0 +1,9 @@
+export const alignN = n => index => {
+  const r = index % n;
+  return r === 0 ? index : (index + n - r);
+};
+export const align4 = alignN(4);
+
+//
+
+export const getClosestPowerOf2 = size => Math.ceil(Math.log2(size));
