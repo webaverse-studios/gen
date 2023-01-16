00850fe4eb0b946bdd5bc2e553c0734cd9732613
Avaer Kazmer <a@modules.io>
2023-01-16 01:45:58 -0500

    Add SceneGraphMesh updateTextureAtlas method

--- src/components/generators/MetasceneGeneratorComponent.jsx
+++ src/components/generators/MetasceneGeneratorComponent.jsx
@@ -1561,6 +1561,18 @@ class SceneGraphMesh extends THREE.InstancedMesh {
     }
     this.instanceMatrix.needsUpdate = true;
   }
+  async updateTextureAtlas() {
+    const canvas = this.material.uniforms.map.value.image;
+
+    const promises = [];
+    for (let index = 0; index < this.panelSpecs.length; index++) {
+      const panelSpec = this.panelSpecs[index];
+      const p = drawAtlasTexture(panelSpec, index, canvas.ctx);
+      promises.push(p);
+    }
+    await Promise.all(promises);
+    this.material.uniforms.map.value.needsUpdate = true;
+  }
 }
 
 //
