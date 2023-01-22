be6299d864972128c78b0efd67f82adea927b3dd
Avaer Kazmer <a@modules.io>
2023-01-22 10:51:21 -0500

    Add missing id-allocator.js

---
+++ id-allocator.js
@@ -0,0 +1,26 @@
+const defaultMaxId = 8192;
+
+export class IdAllocator {
+  constructor(maxSize = defaultMaxId) {
+    this.stack = new Uint32Array(maxSize);
+    for (let i = 0; i < maxSize; i++) {
+      this.stack[i] = i + 1;
+    }
+    this.stackIndex = 0;
+  }
+
+  alloc() {
+    if (this.stackIndex < this.stack.length) {
+      const index = this.stack[this.stackIndex];
+      this.stackIndex++;
+      return index;
+    } else {
+      return -1;
+    }
+  }
+
+  free(index) {
+    this.stackIndex--;
+    this.stack[this.stackIndex] = index;
+  }
+}
