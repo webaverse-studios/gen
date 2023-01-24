ba719152cad0b469768291196fcdcfc94226cd25
Avaer Kazmer <a@modules.io>
2023-01-24 15:47:21 -0500

    Add intitial story engine source

---
+++ src/story-engine/story-engine.js
@@ -0,0 +1,74 @@
+import {makeId} from './util.js';
+
+//
+
+export class StoryManager {
+  constructor({
+    generators,
+  }) {
+    this.generators = generators;
+
+    const conversation = new NLPConversation();
+    this.conversation = conversation;
+  }
+  async createConversationAsync() {
+    let [
+      character1,
+      // character2,
+      setting,
+    ] = await Promise.all([
+      this.generators.dataset.generateItem('character', {
+        // Name: 'Death Mountain',
+        // Description: panelSpec.description,
+      }, {
+        keys: ['Name', 'Description', 'Image'],
+      }),
+      // datasetGenerator.generateItem('character', {
+      //   // Name: 'Death Mountain',
+      //   // Description: panelSpec.description,
+      // }, {
+      //   // keys: ['Name', 'Description', 'Image'],
+      // }),
+      this.generators.dataset.generateItem('setting', {
+        // Name: 'Death Mountain',
+        // Description: panelSpec.description,
+      }, {
+        keys: ['Name', 'Description', 'Image'],
+      }),
+    ]);
+    const formatObject = c => {
+      const result = {};
+      for (const key in c) {
+        result[key.toLowerCase()] = c[key];
+      }
+      return result;
+    };
+    const characters = [
+      character1,
+      // character2,
+    ].map(c =>  formatObject(c));
+    setting = formatObject(setting);
+    // console.log('got character spec', {characters, setting});
+
+    return new NLPConversation({
+      characters,
+      setting,
+    });
+  }
+}
+
+//
+
+export class NLPConversation {
+  constructor({
+    name = `conversation_${makeId()}`,
+    characters = [],
+    setting = '',
+    messages = [],
+  } = {}) {
+    this.name = name;
+    this.characters = characters;
+    this.setting = setting;
+    this.messages = messages;
+  }
+}
