d4e611d874492cb5d570f2ceea2586d4d5aea323
Avaer Kazmer <a@modules.io>
2023-01-25 00:26:19 -0500

    Image cache optimization

--- src/story-engine/story-engine.js
+++ src/story-engine/story-engine.js
@@ -65,11 +65,9 @@ const formatObject = c => {
 
 //
 
-const compileImages = async (text, opts) => {
-  const {
-    abortController = null,
-  } = opts;
-  
+const getMarkdownImagesAsync = async (text, {
+  abortController = null,
+} = {}) => {
   const rootEl = document.createElement('div');
   const root = ReactDOMClient.createRoot(rootEl);
   const p = makePromise();
@@ -86,9 +84,11 @@ const compileImages = async (text, opts) => {
   await p;
   if (abortController && abortController.signal.aborted) {
     throw abortError;
-  }
-
+  }  
   const imgPlaceholders = Array.from(rootEl.querySelectorAll('img'));
+  
+  root.unmount();
+  
   const imgPromptsAlts = imgPlaceholders.map(img => {
     const alt = img.getAttribute('alt');
     const match = alt.match(/^([^\|]*?)\|([\s\S]*)$/);
@@ -103,39 +103,104 @@ const compileImages = async (text, opts) => {
       throw new Error('invalid alt text: ' + alt);
     }
   });
-  const imgs = await Promise.all(imgPromptsAlts.map(async ([promptText, altText]) => {
-    const img = await imageAiClient.createImage(promptText);
-    img.setAttribute('prompt', promptText);
-    img.setAttribute('alt', altText);
-    return img;
+  return imgPromptsAlts;
+}
+const compileImages = async (text, {
+  abortController = null,
+} = {}) => {
+  const imgPromptsAlts = await getMarkdownImagesAsync(text, {
+    abortController,
+  });
+  const imgUrls = await Promise.all(imgPromptsAlts.map(async ([promptText, altText]) => {
+    const imgBlob = await imageAiClient.createImageBlob(promptText);
+    // img.setAttribute('prompt', promptText);
+    // img.setAttribute('alt', altText);
+    // return img;
+    // return {
+    //   imgBlob,
+    // };
+    const url = URL.createObjectURL(imgBlob);
+    return {
+      url,
+      prompt: promptText,
+      alt: altText,
+    };
   }));
   if (abortController && abortController.signal.aborted) {
     throw abortError;
   }
+  return imgUrls;
+};
+const getMainImagePromptsAsync = async (object, {
+  abortController = null,
+} = {}) => {
+  const results = {};
+  for (const type in object) {
+    const items = object[type];
 
-  root.unmount();
-
-  return imgs;
+    results[type] = await Promise.all(items.map(async item => {
+      let result = null;
+      console.log('check item image 1', item);
+      if (item.image) {
+        const imgPromptsAlts = await getMarkdownImagesAsync(item.image, {
+          abortController,
+        });
+        console.log('check item image 2', imgPromptsAlts);
+        if (imgPromptsAlts.length > 0) {
+          const [
+            promptText,
+            altText,
+          ] = imgPromptsAlts[0];
+          result = promptText;
+        }
+      }
+      return result;
+    }));
+  }
+  return results;
 };
 const compileObject = async (object, {
   abortController = null,
 } = {}) => {
   const text = md.toMarkdownString(object);
+  const mainImagePromptAlts = await getMainImagePromptsAsync(object);
+  console.log('main image prompt alts', object, mainImagePromptAlts);
+  const mainImagePrompts = (() => {
+    const results = {};
+    for (const type in mainImagePromptAlts) {
+      results[type] = [];
+
+      const items = mainImagePromptAlts[type];
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        
+        let result = null;
+        if (item) {
+          const [promptText, altText] = item;
+          result = promptText;
+        }
+        results[type].push(result);
+      }
+    }
+    return results;
+  })();
+  console.log('main image prompts', mainImagePrompts);
   const [
-    imgs,
+    images,
     vector,
   ] = await Promise.all([
     compileImages(text, {
       abortController,
     }),
     aiClient.embed(text, {
-      // abortController,
+      abortController,
     }),
   ]);
 
   return {
+    mainImagePrompts,
     text,
-    imgs,
+    images,
     vector,
   };
 };
@@ -207,7 +272,7 @@ export class NLPConversation {
   }
   async updateAsync() {
     const {
-      imgs,
+      images,
       text,
       vector,
     } = await compileObject({
@@ -215,23 +280,32 @@ export class NLPConversation {
       setting: [this.setting],
     });
 
-    for (let i = 0; i < imgs.length; i++) {
-      const img = imgs[i];
-      const prompt = img.getAttribute('prompt');
-      this.imageCache.set(prompt, img);
-    }
-    this.vector = vector;
+    for (let i = 0; i < images.length; i++) {
+      const {
+        url,
+        prompt,
+        alt,
+      } = images[i];
 
-    // XXX debug display
-    for (let i = 0; i < imgs.length; i++) {
-      const img = imgs[i];2
+      const img = document.createElement('img');
+      img.src = url;
+      img.setAttribute('prompt', prompt);
+      img.setAttribute('alt', alt);
       img.style.cssText = `\
         width: 512px;
         height: 512px;
         background: red;
       `;
       document.body.appendChild(img);
+      await new Promise((accept, reject) => {
+        img.onload = accept;
+        img.onerror = reject;
+      });
+      this.imageCache.set(prompt, img);
     }
-    console.log('got image cache', {imgs, text, vector}, this.imageCache, this.vector);
+    this.vector = vector;
+
+    // XXX debug display
+    console.log('got image cache', {images, text, vector}, this.imageCache, this.vector);
   }
 }
