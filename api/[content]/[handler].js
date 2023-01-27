68a464b0d51dbb7ed380601482ff5cb8b4b3d978
Avaer Kazmer <a@modules.io>
2023-01-26 23:01:38 -0500

    Update handler to use AI server

--- api/[content]/[handler].js
+++ api/[content]/[handler].js
@@ -1,7 +1,14 @@
+import url from 'url';
+import {AiServer} from '../../src/servers/ai-server.js'; 
+
+const aiServer = new AiServer();
+
 export default function handler(request, response) {
-  response.status(200).json({
-    body: 'Hello content: ' + request.url,
-    // query: request.query,
-    // cookies: request.cookies,
-  });
+  handleRequest.handleRequest(request, response);  
+  
+  // response.status(200).json({
+  //   body: 'Hello content: ' + request.url,
+  //   // query: request.query,
+  //   // cookies: request.cookies,
+  // });
 }
