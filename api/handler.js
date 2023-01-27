407bc9aeeb9588d74e7d9c21fd2a9ad8ed7b91b6
Avaer Kazmer <a@modules.io>
2023-01-26 23:01:17 -0500

    Remove base handler.js

--- api/handler.js
+++
@@ -1,7 +0,0 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    body: 'Hello serverless: ' + request.url,
-    // query: request.query,
-    // cookies: request.cookies,
-  });
-}
