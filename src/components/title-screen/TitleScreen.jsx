22b98855e89d7652cb39ff34da9e27b0deafff48
Avaer Kazmer <a@modules.io>
2023-01-26 23:31:08 -0500

    Use scene gallery component on title screen

--- src/components/title-screen/TitleScreen.jsx
+++ src/components/title-screen/TitleScreen.jsx
@@ -1,10 +1,9 @@
 import * as THREE from 'three';
-import React, {useState, useEffect, useRef, useContext, createContext, Fragment} from 'react';
+import {useState, useEffect, useRef, useContext, createContext, Fragment} from 'react';
 import classnames from 'classnames';
 
 //
 
-// import '../../../styles/globals.css';
 import styles from '../../../styles/TitleScreen.module.css';
 
 //
@@ -24,13 +23,9 @@ import {
 import {
     PathMesh,
 } from '../../zine-aux/meshes/path-mesh.js';
-
-//
-
-const contentBaseUrl = `https://cdn.jsdelivr.net/gh/webaverse/content@main/`;
-const imagesUrl = `${contentBaseUrl}images/`;
-const imagesListUrl = `${imagesUrl}list.txt`;
-// const assetsBaseUrl = `https://webaverse.github.io/gwe-assets`;
+import{
+    SceneGallery,
+} from '../image-gallery/SceneGallery.jsx';
 
 //
 
@@ -252,105 +247,6 @@ const _startApp = (canvas, u) => {
 
 //
 
-const GalleryImagePlaceholder = ({
-    imgPlaceholder,
-    onIntersect,
-}) => {
-    const ref = useRef();
-
-    useEffect(() => {
-        const el = ref.current;
-        if (el) {
-            const options = {
-                root: null,
-                rootMargin: '1000px',
-                threshold: 0,
-            };
-            const callback = (entries, observer) => {
-                let isIntersecting = false;
-                for (const entry of entries) {
-                    if (entry.isIntersecting) {
-                        isIntersecting = true;
-                        break;
-                    }
-                }
-                isIntersecting && onIntersect();
-            };
-            let observer = new IntersectionObserver(callback, options);
-            observer.observe(el);
-
-            return () => {
-                observer.disconnect();
-            };
-        }
-    }, [imgPlaceholder, ref.current]);
-
-    return (
-        <div className={styles.imageWrap} ref={ref} />
-    );
-};
-
-//
-
-const Gallery = ({
-    onImageClick,
-}) => {
-    const [imgPlaceholders, setImgPlaceholders] = useState(0);
-    const [imgUrls, setImgUrls] = useState([]);
-    
-    useEffect(() => {
-        let live = true;
-        
-        (async () => {
-            const res = await fetch(imagesListUrl);
-            if (!live) return;
-            
-            const text = await res.text();
-            if (!live) return;
-
-            const urls = text.split('\n')
-              .map(name => `${imagesUrl}${name}`);
-            // console.log('load new urls', urls);
-            setImgUrls(urls);
-        })();
-
-        return () => {
-            live = false;
-        };
-    }, []);
-
-    const numImgPlaceholdersPerChunk = 16;
-
-    return (
-        <div className={styles.gallery}>
-            {(() => {
-                const results = Array(imgPlaceholders);
-                for (let i = 0; i < imgPlaceholders; i++) {
-                    const u = imgUrls[i];
-                    results[i] = u ? (
-                        <GalleryImage
-                            src={u}
-                            onClick={e => {
-                                    onImageClick(u);
-                            }}
-                            key={i}
-                        />
-                    ) : null;
-                }
-                return results;
-            })()}
-            <GalleryImagePlaceholder
-                imgPlaceholder={imgPlaceholders}
-                onIntersect={e => {
-                    setImgPlaceholders(imgPlaceholders + numImgPlaceholdersPerChunk);
-                }}
-            />
-        </div>
-    );
-};
-
-//
-
 const MainScreen = ({
     appStarted,
     canvasRef,
@@ -366,99 +262,6 @@ const MainScreen = ({
             <footer className={styles.footer}>
                 <span className={styles.bold}>SEVERE WARNING:</span> This product is not intended for children under age sixty. <span className={styles.bold}>This is an AI generated product.</span> The ideas expressed are not proven to be safe. This product contains course language and due to its nature it should be viewed twice. Made by the Lisk.
             </footer>
-        </div>
-    );
-};
-
-//
-
-const GalleryImage = ({
-    src,
-    onClick,
-}) => {
-    const ref = useRef();
-    const imgRef = useRef();
-
-    useEffect(() => {
-        const el = ref.current;
-        const imgEl = imgRef.current;
-        if (el && imgEl) {
-            const options = {
-                root: null,
-                rootMargin: '1000px',
-                threshold: 0,
-            };
-            const callback = (entries, observer) => {
-                // console.log('got entries', {
-                //     entries,
-                //     observer,
-                // });
-                for (const entry of entries) {
-                    if (entry.isIntersecting) {
-                        if (!imgEl.loaded) {
-                            imgEl.loaded = true;
-                            // console.log('intersecting', entry, imgEl.src);
-                            /* const img = new Image();
-                            img.src = src;
-                            img.crossOrigin = 'Anonymous';
-                            img.onload = () => {
-                                console.log('got image', img);
-                                el.appendChild(img);
-                            };
-                            observer.unobserve(entry.target); */
-
-                            imgEl.classList.add(styles.loaded);
-
-                            const load = e => {
-                                // imgEl.classList.add(styles.loaded);
-                                cleanup();
-                            };
-                            imgEl.addEventListener('load', load);
-                            const error = e => {
-                                imgEl.classList.add(styles.errored);
-                                cleanup();
-                            };
-                            imgEl.addEventListener('error', error);
-                            const cleanup = () => {
-                                imgEl.removeEventListener('load', load);
-                                imgEl.removeEventListener('error', error);
-                            };
-                            imgEl.src = src;
-                        }
-                    } else {
-                        if (imgEl.loaded) {
-                            // compute the scroll parent
-                            // const scrollTop = window.pageYOffset;
-
-                            // compute the scroll distance
-                            // const rect = imgEl.getBoundingClientRect();                            
-                            // const scrollDistance = Math.max(0, -rect.bottom, -rect.top);
-                            // console.log('scroll', scrollDistance);
-
-                            // if (scrollDistance > 1000) {
-                                // console.log('scroll distance cleared', scrollDistance);
-                                imgEl.removeAttribute('src');
-                                // imgEl.src = '';
-                                imgEl.loaded = false;
-                                imgEl.classList.remove(styles.loaded);
-                                imgEl.classList.remove(styles.errored);
-                            // }
-                        }
-                    }
-                }
-            };
-            let observer = new IntersectionObserver(callback, options);
-            observer.observe(el);
-
-            return () => {
-                observer.disconnect();
-            };
-        }
-    }, [ref.current, imgRef.current]);
-
-    return (
-        <div className={styles.imageWrap} ref={ref}>
-            <img crossOrigin="Anonymous" className={styles.image} onClick={onClick} ref={imgRef} />
         </div>
     );
 };
@@ -482,7 +285,7 @@ const TitleScreen = () => {
                 appStarted={appStarted}
                 canvasRef={canvasRef}
             />
-            {appStarted ? null : <Gallery
+            {appStarted ? null : <SceneGallery
                 onImageClick={u => {
                     if (canvasRef.current && !appStarted) {
                         setAppStarted(true);
