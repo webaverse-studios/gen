b0183135653dc4cd2a5a755bf91636afebf1d522
Avaer Kazmer <a@modules.io>
2023-01-26 22:13:34 -0500

    Update handler API to return url

--- api/handler.js
+++ api/handler.js
@@ -1,6 +1,6 @@
 export default function handler(request, response) {
   response.status(200).json({
-    body: 'Hello serverless',
+    body: 'Hello serverless: ' + request.url,
     // query: request.query,
     // cookies: request.cookies,
   });
