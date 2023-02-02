a23b0791f4451c07c22bafaecc995f609b165ccb
Avaer Kazmer <a@modules.io>
2023-02-02 01:50:06 -0500

    Add physics directory

---
+++ src/physics/constants.js
@@ -0,0 +1,6 @@
+export const heightfieldScale = 0.1; // must fit heightfield in int16
+
+// note: For WASM API.
+export const GET = 0;
+export const GET_NORMALIZED = 1;
+export const GET_INVERSE = 2;
