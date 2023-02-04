6d045419afd21f9f9e354c9e44e63efcd9c805eb
Avaer Kazmer <a@modules.io>
2023-02-04 17:09:42 -0500

    Use imported entrance/exit mesh

--- src/generators/scene-generator.js
+++ src/generators/scene-generator.js
@@ -148,7 +148,12 @@ import {
   getDepthRenderSpecsMeshes,
   renderMeshesDepth,
 } from '../clients/reconstruction-client.js';
-import {PathMesh} from '../zine-aux/meshes/path-mesh.js';
+import {
+  EntranceExitMesh,
+} from '../zine-aux/meshes/entrance-exit-mesh.js';
+import {
+  PathMesh,
+} from '../zine-aux/meshes/path-mesh.js';
 // import '../../lore-test.js';
 
 //
@@ -2657,61 +2662,6 @@ class PortalNetMesh extends THREE.Mesh {
     portalNetMesh.visible = false;
     portalNetMesh.updateVisibility = () => {
       portalNetMesh.visible = portalNetMesh.enabled && hasGeometry;
-    };
-  }
-}
-
-//
-
-class EntranceExitMesh extends THREE.Mesh {
-  constructor({
-    entranceExitLocations,
-  }) {
-    const baseGeometry = new THREE.BoxGeometry(entranceExitWidth, entranceExitHeight, entranceExitDepth)
-      .translate(0, entranceExitHeight / 2, entranceExitDepth / 2);
-    const geometries = entranceExitLocations.map(portalLocation => {
-      const g = baseGeometry.clone();
-      g.applyMatrix4(
-        localMatrix.compose(
-          localVector.fromArray(portalLocation.position),
-          localQuaternion.fromArray(portalLocation.quaternion),
-          localVector2.setScalar(1)
-        )
-      );
-      return g;
-    });
-    const geometry = geometries.length > 0 ? BufferGeometryUtils.mergeBufferGeometries(geometries) : new THREE.BufferGeometry();
-
-    const material = new THREE.ShaderMaterial({
-      vertexShader: `\
-        varying vec2 vUv;
-
-        void main() {
-          vUv = uv;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }
-      `,
-      fragmentShader: `\
-        varying vec2 vUv;
-
-        void main() {
-          vec3 c = vec3(1., 0., 1.);
-          gl_FragColor = vec4(c, 0.5);
-          gl_FragColor.rg += vUv * 0.2;
-        }
-      `,
-      transparent: true,
-    });
-    super(geometry, material);
-
-    const hasGeometry = geometries.length > 0;
-
-    const entranceExitMesh = this;
-    entranceExitMesh.frustumCulled = false;
-    entranceExitMesh.enabled = false;
-    entranceExitMesh.visible = false;
-    entranceExitMesh.updateVisibility = () => {
-      entranceExitMesh.visible = entranceExitMesh.enabled && hasGeometry;
     };
   }
 }
