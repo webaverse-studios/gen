5fc44a1bdca2d35ddb78d27c1bcf2255b2337583
Avaer Kazmer <a@modules.io>
2023-02-05 16:02:48 -0500

    Import Quest.jsx

--- src/components/title-screen/TitleScreen.jsx
+++ src/components/title-screen/TitleScreen.jsx
@@ -112,6 +112,10 @@ import {
     DatasetGenerator,
 } from '../../dataset-engine/dataset-generator.js';
 
+import {
+    Quest,
+} from './Quest.jsx';
+
 import {AiClient} from '../../../clients/ai/ai-client.js';
 
 //
@@ -1401,69 +1405,6 @@ const MainScreen = ({
 //
 
 const seenRouters = new WeakMap();
-
-//
-
-const Quest = ({
-    Name,
-    Description,
-    Image,
-    Objectives,
-    live = true,
-    onZombie = () => {},
-}) => {
-    const [animateIn, setAnimateIn] = useState(false);
-    const [animateOut, setAnimateOut] = useState(false);
-    const nodeRef = useRef(null);
-
-    useEffect(() => {
-        if (!animateIn) {
-            const frame = requestAnimationFrame(() => {
-                setAnimateIn(true);
-            });
-            return () => {
-                cancelAnimationFrame(frame);
-            };
-        }
-    }, [animateIn]);
-    useEffect(() => {
-        if (nodeRef.current && !live) {
-            console.log('set animate out', true);
-            setAnimateOut(true);
-
-            const nodeEl = nodeRef.current;
-            const transitionEnd = e => {
-                if (e.target === nodeEl) {
-                    nodeEl.removeEventListener('transitionend', transitionEnd);
-                    onZombie();
-                }
-            };
-            nodeEl.addEventListener('transitionend', transitionEnd);
-
-            return () => {
-                nodeEl.removeEventListener('transitionend', transitionEnd);
-            };
-        }
-    }, [nodeRef.current, live]);
-
-    const animate = animateIn && !animateOut;
-    return (<div className={classnames(
-        styles.quest,
-        animate ? styles.animate : null,
-    )} ref={nodeRef}>
-        <div className={styles.content}>
-            <div className={styles.name}>
-                {Name}
-            </div>
-            <div className={styles.description}>
-                {Description}
-            </div>
-            <div className={styles.objectives}>
-                {Objectives.join('\n')}
-            </div>
-        </div>
-    </div>);
-}
 
 //
 
